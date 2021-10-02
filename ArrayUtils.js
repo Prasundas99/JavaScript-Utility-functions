1a5750cb6eef8e54b5c86a9c0c401d15a7519435
Prasun Das <58937669+Prasundas99@users.noreply.github.com>
2021-10-02 23:02:11 +0530

    Revert "Added countOccurrences function in ArrayUtils"

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -1,32 +1,30 @@
 var titleCaseString = (data) =>
   data
     .toLowerCase()
-    .split(" ")
+    .split(' ')
     .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
-    .join(" ");
+    .join(' ')
 
 var convertStringToArray = (para, removeSpaces = false) =>
-  removeSpaces ? [...para].filter((item) => item !== " ") : [...para];
+  removeSpaces ? [...para].filter((item) => item !== ' ') : [...para]
 
 var countInstanceInArray = (arr) =>
   arr.reduce((obj, item) => {
-    if (!obj[item]) obj[item] = 0;
-    obj[item]++;
-    return obj;
-  }, {});
+    if (!obj[item]) obj[item] = 0
+    obj[item]++
+    return obj
+  }, {})
 
 var sumOfAnArray = (arr, initialValue = 0) =>
-  arr.reduce((a, b) => a + b, initialValue);
+  arr.reduce((a, b) => a + b, initialValue)
 
-var inArrayOfObjects = (arr, key, value) => arr.find((ar) => ar[key] === value);
+var inArrayOfObjects = (arr, key, value) => arr.find((ar) => ar[key] === value)
 
 var findArrayIndex = (arr, key, value) =>
-  arr.findIndex((ar) => ar[key] === value);
+  arr.findIndex((ar) => ar[key] === value)
 
 var getUniqueList = (arr) =>
   arr.filter(
     (ar, index, list) =>
       list.findIndex((p) => JSON.stringify(ar) === JSON.stringify(p)) === index
-  );
-var countOccurrences = (arr, value) =>
-  arr.reduce((a, v) => (v === value ? a + 1 : a + 0), 0);
+  )
