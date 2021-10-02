a7bdd1e160b3c6639bfce53f2d1dd43b59c584c4
Soham Saha <sahasoham01@gmail.com>
2021-10-02 20:54:56 +0530

    Function to count number of vowels and consonants

---
+++ countvowelsandconsonants.js
@@ -0,0 +1,16 @@
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+function countVowels(sentence) {
+  let vowelcounts = 0;
+  let consocounts = 0;
+  for(let i = 0; i < vowels.length; i++) {
+    if(vowels.includes(sentence[i])) {
+      vowelcounts++;
+    }
+    else
+    {
+        consocounts++;
+    }
+  }
+  return console.log(vowelcounts,consocounts);
+}
