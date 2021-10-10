b9773c7c339db24a144ae2d58c35bd126778da7e
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2021-10-10 10:22:37 +0530

    Insertion sort using js.

---
+++ insertionSort.js
@@ -0,0 +1,18 @@
+const insertionSort = arr =>
+  arr.reduce((acc, x) => {
+    if (!acc.length) return [x];
+    acc.some((y, j) => {
+      if (x <= y) {
+        acc.splice(j, 0, x);
+        return true;
+      }
+      if (x > y && j === acc.length - 1) {
+        acc.splice(j + 1, 0, x);
+        return true;
+      }
+      return false;
+    });
+    return acc;
+  }, []);
+ 
+console.log(insertionSort([6, 3, 4, 1]));
