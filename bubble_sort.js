99d2fd31541f0ed88f35af2c943fcf2b9071c8ab
subh839 <79743346+subh839@users.noreply.github.com>
2022-10-05 22:24:49 +0530

    function

---
+++ bubble_sort.js
@@ -0,0 +1,36 @@
+// Optimized implementation of bubble sort Algorithm
+
+function bubbleSort(arr){
+
+    var i, j;
+    var len = arr.length;
+    
+    var isSwapped = false;
+    
+    for(i =0; i < len; i++){
+        
+        isSwapped = false;
+        
+        for(j = 0; j < len; j++){
+            if(arr[j] > arr[j + 1]){
+            var temp = arr[j]
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+            isSwapped = true;
+            }
+        }
+        
+        // IF no two elements were swapped by inner loop, then break
+        
+        if(!isSwapped){
+        break;
+        }
+    }
+    
+    // Print the array
+    console.log(arr)
+    }
+    
+    
+
+    
