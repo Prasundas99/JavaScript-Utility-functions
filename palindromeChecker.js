d86d70e698cb7d520246ae247f4b4aba0695c2d1
Sanchayeeta Saha <68606762+noobsanchu69@users.noreply.github.com>
2022-10-16 16:13:54 +0530

    Create palindromeChecker.js

---
+++ palindromeChecker.js
@@ -0,0 +1,10 @@
+// Defining the function
+const palindrome = (word) => { 
+  // Reverse the word
+  let reverseWord = word.split('').reverse().join('');
+  // Return a Boolean after checking if the reverseWord is strictly equal to word
+  return reverseStr === lowRegStr;
+}
+
+// Calling the function
+palindrome("level");
